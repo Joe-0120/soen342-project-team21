25ab81820f153db4d83c83a39e394f629f23c89e
Youssef Ouakaa <youssef.ouakaa2@gmail.com>
2024-02-25 23:38:10 -0500

    Add airport catalog

--- soen342-code/src/AirportCatalog.java
+++ soen342-code/src/AirportCatalog.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+
 public class AirportCatalog {
-    
+    private ArrayList<Airport> airports = new ArrayList<Airport>();
+
+    public AirportCatalog() {
+    }
+
+    public ArrayList<Airport> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(ArrayList<Airport> airports) {
+        this.airports = airports;
+    }
 }
