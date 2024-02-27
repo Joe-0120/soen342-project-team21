67007c99f3d974cd8cbb39cff72759620d9bdc07
Jonathan <jonathandominicpasquin@gmail.com>
2024-02-26 21:35:55 -0500

    Flight and FlightCatalog Classes

--- soen342-code/src/Flight.java
+++ soen342-code/src/Flight.java
@@ -1,2 +1,57 @@
+import java.time.LocalDateTime;
+
 public class Flight {
+  private String number;
+  private LocalDateTime scheduledDep;
+  private LocalDateTime actualDep;
+  private LocalDateTime scheduledArr;
+  private LocalDateTime estimatedArr;
+
+  public Flight(String number, LocalDateTime scheduledDep, LocalDateTime actualDep, LocalDateTime scheduledArr, LocalDateTime estimatedArr) {
+    this.number = number;
+    this.scheduledDep = scheduledDep;
+    this.actualDep = actualDep;
+    this.scheduledArr = scheduledArr;
+    this.estimatedArr = estimatedArr;
+  }
+
+  public String getNumber() {
+    return number;
+  }
+
+  public void setNumber(String number) {
+    this.number = number;
+  }
+
+  public LocalDateTime getScheduledDep() {
+    return scheduledDep;
+  }
+
+  public void setScheduledDep(LocalDateTime scheduledDep) {
+    this.scheduledDep = scheduledDep;
+  }
+
+  public LocalDateTime getActualDep() {
+    return actualDep;
+  }
+
+  public void setActualDep(LocalDateTime actualDep) {
+    this.actualDep = actualDep;
+  }
+
+  public LocalDateTime getScheduledArr() {
+    return scheduledArr;
+  }
+
+  public void setScheduledArr(LocalDateTime scheduledArr) {
+    this.scheduledArr = scheduledArr;
+  }
+
+  public LocalDateTime getEstimatedArr() {
+    return estimatedArr;
+  }
+
+  public void setEstimatedArr(LocalDateTime estimatedArr) {
+    this.estimatedArr = estimatedArr;
+  }
 }
--- soen342-code/src/FlightCatalog.java
+++ soen342-code/src/FlightCatalog.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlightCatalog {
+  private ArrayList<Flight> flights = new ArrayList<Flight>();
+
+  public FlightCatalog() {
+  }
+
+  public void addFlight(Flight flight) {
+      flights.add(flight);
+  }
+
+  public boolean removeFlight(String number) {
+      return flights.removeIf(flight -> flight.getNumber().equals(number));
+  }
+
+  public List<Flight> getAllFlights() {
+      return new ArrayList<>(flights);
+  }
+
+  public Flight findFlightByNumber(String number) {
+      for (Flight flight : flights) {
+          if (flight.getNumber().equals(number)) {
+              return flight;
+          }
+      }
+      return null;
+  }
 }
