bd831c8d442a770410742b8decdd9783f2e7378d
Youssef Ouakaa <youssef.ouakaa2@gmail.com>
2024-03-26 14:30:54 -0400

    Create CityCatalog class

--- soen342-code/src/CityCatalog.java
+++ soen342-code/src/CityCatalog.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class CityCatalog {
+import java.util.ArrayList;
+
+public class CityCatalog {
+    private ArrayList<City> cities = new ArrayList<>();
+    public CityCatalog(){
+    }
+    public ArrayList<City> getCities() {
+        return cities;
+    }
+
+    public void setCities(ArrayList<City> cities) {
+        this.cities = cities;
+    }
+
+    public City getAirport(String cityName) {
+        for (City city : cities) {
+            if (city.getName().equals(cityName)) {
+                return city;
+            }
+        }
+        return null; // If airport with the given code is not found
+    }
+
+
 }
